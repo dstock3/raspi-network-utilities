27532e7ac7ba2ec250b32e82b21184ac0b4aa917
David Stockdale <dstock3@gmail.com>
2023-02-05 13:28:41 -0600

    imported pihole and dotenv and populated request with password.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hello, World!");
+const dotenv = require('dotenv');
+dotenv.config();
+const PiHole = require('pihole');
+
+(async function test() {
+    const pihole = new PiHole(process.env.WEBPASSWORD)
+    console.log(await pihole.getAllQueries());
+    //returning an array of arrays of DNS queries
+})();
