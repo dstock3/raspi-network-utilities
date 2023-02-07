0779da09ccce74b0b7bbbda9e0199c9ff9973a86
David Stockdale <dstock3@gmail.com>
2023-02-07 16:42:13 -0600

    added some error handling.

--- index.js
+++ index.js
@@ -10,16 +10,29 @@ for (const key in funct.apiFunctions) {
   const thisFunction = funct.apiFunctions[key]
   if (thisFunction.nested) {
     app.get(thisFunction.route, async (req, res) => {
-      let data = await processRoute(thisFunction.name)
-      res.send(data["data"]);
+      try {
+        let data = await processRoute(thisFunction.name)
+        res.send(data["data"]);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send({ error: "An error occurred processing the request" });
+      }
     });
   } else {
     app.get(thisFunction.route, async (req, res) => {
-      let data = await processRoute(thisFunction.name)
-      res.send(data);
+      try {
+        let data = await processRoute(thisFunction.name)
+        res.send(data);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send({ error: "An error occurred processing the request" });
+      }
     });
   }
 }
+
+
+
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
