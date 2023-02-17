2a5c013c64dee70826803ca3b64320fc95ca1b09
David Stockdale <dstock3@gmail.com>
2023-02-16 20:05:10 -0600

    Added new route for internet speed test.

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 const app = express();
 const dotenv = require("dotenv");
 dotenv.config();
+const { exec } = require('child_process');
+const FastSpeedtest = require("fast-speedtest-api");
 const port = process.env.PORT || 3000;
 const funct = require("./functions/apiFunctions");
 const { processRoute, processTryCatch } = require("./functions/helper");
@@ -10,6 +12,26 @@ const { processRoute, processTryCatch } = require("./functions/helper");
 app.use(cors({
   origin: process.env.CORS_ORIGIN
 }));
+
+app.get("/speedtest", async (req, res) => {
+  try {
+    const speedtest = new FastSpeedtest({
+      token: process.env.SPEEDTEST_TOKEN, // required
+      verbose: false, // default: false
+      timeout: 10000, // default: 5000
+      https: true, // default: true
+      urlCount: 5, // default: 5
+      bufferSize: 8, // default: 8
+      unit: FastSpeedtest.UNITS.Mbps, // default: Bps
+    });
+
+    const speed = await speedtest.getSpeed();
+    const roundedSpeed = Math.round(speed);
+    res.send({ speed: `${roundedSpeed} Mbps` });
+  } catch (error) {
+    res.status(500).send({ error: "An error occurred processing the request" });
+  }
+})
 
 for (const key in funct.apiFunctions) {
   const thisFunction = funct.apiFunctions[key]
